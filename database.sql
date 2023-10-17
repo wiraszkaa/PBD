879c990ef88e9e52d3e9ef327f24853919175558
Jakub Wiraszka <WirachaPL@wp.pl>
2023-10-17 21:50:36 +0200

    Custom Types

--- database.sql
+++ database.sql
@@ -1,25 +1,32 @@
+CREATE DOMAIN Pesel AS varchar(11) NOT NULL CHECK (value ~ '^[0-9]{11}$');
+CREATE DOMAIN Money AS decimal(9,2) NOT NULL CHECK(value >= 0);
+CREATE DOMAIN Email AS varchar(50) NOT NULL CHECK (value ~ '^[^@]+@[^\.@]+\..+$');
+CREATE DOMAIN Password AS varchar(50) NOT NULL CHECK (value ~ '^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]{8,}$');
+CREATE DOMAIN PhoneNumber AS varchar(9) NOT NULL CHECK (value ~ '^[0-9]{9}$');
+CREATE TYPE UserRole AS ENUM ('user', 'employee');
+
 CREATE TABLE "Sector" (
-  "SectorId" integer PRIMARY KEY,
+  "SectorId" bigserial PRIMARY KEY,
   "SectorName" varchar(50) NOT NULL,
   "SectorDescription" varchar(250),
-  "FK_FaithId" integer
+  "FK_FaithName" varchar(50)
 );
 
 CREATE TABLE "Faith" (
   "FaithName" varchar(50) PRIMARY KEY,
   "FaithDescription" varchar(100) NOT NULL,
-  "NumberOfBelievers" integer
+  "NumberOfBelievers" integer CHECK ("NumberOfBelievers" > 0)
 );
 
 CREATE TABLE "Monument" (
-  "MonumentId" integer PRIMARY KEY,
+  "MonumentId" bigserial PRIMARY KEY,
   "MonumentName" varchar(50) NOT NULL,
   "MonumentDescription" varchar(250),
   "FK_SectorId" integer NOT NULL
 );
 
 CREATE TABLE "Gate" (
-  "GateId" integer PRIMARY KEY,
+  "GateId" bigserial PRIMARY KEY,
   "GateName" varchar(50) NOT NULL,
   "GateDescription" varchar(250),
   "OpeningHours" varchar(20),
@@ -27,7 +34,7 @@ CREATE TABLE "Gate" (
 );
 
 CREATE TABLE "Camera" (
-  "CameraId" integer PRIMARY KEY,
+  "CameraId" bigserial PRIMARY KEY,
   "CameraModel" varchar(50) NOT NULL,
   "CameraManufacturer" varchar(50),
   "InstallationDate" date NOT NULL,
@@ -36,7 +43,7 @@ CREATE TABLE "Camera" (
 );
 
 CREATE TABLE "Grave" (
-  "GraveId" integer PRIMARY KEY,
+  "GraveId" bigserial PRIMARY KEY,
   "GraveType" varchar(20) NOT NULL,
   "GraveStatus" varchar(10) NOT NULL,
   "PaymentExpirationDate" date,
@@ -44,14 +51,14 @@ CREATE TABLE "Grave" (
 );
 
 CREATE TABLE "Chapel" (
-  "ChapelId" integer PRIMARY KEY,
+  "ChapelId" bigserial PRIMARY KEY,
   "ChapelName" varchar(50) NOT NULL,
   "ArchitecturalStyle" varchar(50),
   "FK_SectorId" integer NOT NULL
 );
 
 CREATE TABLE "Funeral" (
-  "FuneralId" integer PRIMARY KEY,
+  "FuneralId" bigserial PRIMARY KEY,
   "FuneralDate" date NOT NULL,
   "FuneralDescription" varchar(250),
   "FK_ChapelId" integer NOT NULL,
@@ -60,114 +67,94 @@ CREATE TABLE "Funeral" (
 );
 
 CREATE TABLE "Deceased" (
-  "DeceasedId" integer PRIMARY KEY,
+  "DeceasedId" bigserial PRIMARY KEY,
   "DeceasedFirstName" varchar(20) NOT NULL,
   "DeceasedLastName" varchar(30) NOT NULL,
   "DateOfDeath" date NOT NULL,
-  "DateOfBirth" date NOT NULL,
+  "DateOfBirth" date NOT NULL CHECK ("DateOfBirth" < "DateOfDeath"),
   "CauseOfDeath" varchar(50),
-  "DeceasedPESEL" varchar(11) UNIQUE NOT NULL,
+  "DeceasedPESEL" Pesel UNIQUE,
   "FK_GraveId" integer NOT NULL,
   "FK_FaithName" varchar(50) NOT NULL
 );
 
 CREATE TABLE "Payment" (
-  "PaymentId" integer PRIMARY KEY,
-  "Amount" float NOT NULL,
-  "PaymentDate" date NOT NULL,
+  "PaymentId" bigserial PRIMARY KEY,
+  "Amount" Money,
+  "PaymentDate" date NOT NULL CHECK ("PaymentDate" < date('now')),
   "PaymentStatus" varchar(10) NOT NULL,
   "PaymentMethod" varchar(20) NOT NULL,
   "PaymentDescription" varchar(200)
 );
 
 CREATE TABLE "Reservation" (
-  "ReservationId" integer PRIMARY KEY,
+  "ReservationId" bigserial PRIMARY KEY,
   "ReservationDescription" varchar(150) NOT NULL,
   "ReservationDate" date NOT NULL,
-  "ReservationExpirationDate" date NOT NULL,
+  "ReservationExpirationDate" date NOT NULL CHECK ("ReservationExpirationDate" > "ReservationDate"),
   "ReservationStatus" varchar(20) NOT NULL,
   "FK_UserId" integer NOT NULL,
   "FK_GraveId" integer UNIQUE NOT NULL,
   "FK_PaymentId" integer UNIQUE NOT NULL
 );
 
+
 CREATE TABLE "User" (
-  "UserId" integer PRIMARY KEY,
+  "UserId" bigserial PRIMARY KEY,
   "UserFirstName" varchar(20) NOT NULL,
   "UserLastName" varchar(30) NOT NULL,
-  "UserEmail" varchar(50) UNIQUE NOT NULL,
-  "UserPassword" varchar(50) NOT NULL,
-  "UserPhoneNumber" varchar(10) NOT NULL,
-  "UserRole" varchar(20) NOT NULL
+  "UserEmail" Email UNIQUE,
+  "UserPassword" Password,
+  "UserPhoneNumber" PhoneNumber,
+  "UserRole" UserRole
 );
 
 CREATE TABLE "Service" (
-  "ServiceId" integer PRIMARY KEY,
+  "ServiceId" bigserial PRIMARY KEY,
   "ServiceName" varchar(20) NOT NULL,
   "ServiceDescription" varchar(100) NOT NULL,
-  "ServicePrice" float NOT NULL
+  "ServicePrice" Money
 );
 
 CREATE TABLE "DeceasedHistory" (
-  "DeceasedHistoryId" integer PRIMARY KEY,
+  "DeceasedHistoryId" bigserial PRIMARY KEY,
   "HistoryDescription" varchar(500) NOT NULL,
   "DateAdded" date NOT NULL,
-  "FK_DeceasedId" integer UNIQUE,
+  "FK_DeceasedId" integer UNIQUE
 );
 
 CREATE TABLE "Subscription" (
-  "SubscriptionId" integer PRIMARY KEY,
+  "SubscriptionId" bigserial PRIMARY KEY,
+  "ServiceStartTime" date NOT NULL,
+  "ServiceEndTime" date NOT NULL CHECK ("ServiceEndTime" > "ServiceStartTime"),
   "FK_GraveId" integer NOT NULL,
   "FK_ServiceId" integer NOT NULL,
-  "ServiceStartTime" date NOT NULL,
-  "ServiceEndTime" date NOT NULL,
   "FK_PaymentId" integer UNIQUE
 );
 
 CREATE TABLE "Purchase" (
-  "PurchaseId" integer PRIMARY KEY,
-  "FK_UserId" integer,
-  "FK_ServiceId" integer NOT NULL
+  "PurchaseId" bigserial PRIMARY KEY,
+  "FK_ServiceId" integer NOT NULL,
+  "FK_UserId" integer
 );
 
-ALTER TABLE "Sector" ADD FOREIGN KEY ("FaithId") REFERENCES "Faith" ("FaithName");
-
-ALTER TABLE "Monument" ADD FOREIGN KEY ("SectorId") REFERENCES "Sector" ("SectorId");
-
-ALTER TABLE "Gate" ADD FOREIGN KEY ("SectorId") REFERENCES "Sector" ("SectorId");
-
-ALTER TABLE "Camera" ADD FOREIGN KEY ("SectorId") REFERENCES "Sector" ("SectorId");
-
-ALTER TABLE "Grave" ADD FOREIGN KEY ("SectorId") REFERENCES "Sector" ("SectorId");
-
-ALTER TABLE "Chapel" ADD FOREIGN KEY ("SectorId") REFERENCES "Sector" ("SectorId");
-
-ALTER TABLE "Funeral" ADD FOREIGN KEY ("ChapelId") REFERENCES "Chapel" ("ChapelId");
-
-ALTER TABLE "Funeral" ADD FOREIGN KEY ("DeceasedId") REFERENCES "Deceased" ("DeceasedId");
-
-ALTER TABLE "Funeral" ADD FOREIGN KEY ("PaymentId") REFERENCES "Payment" ("PaymentId");
-
-ALTER TABLE "Deceased" ADD FOREIGN KEY ("GraveId") REFERENCES "Grave" ("GraveId");
-
-ALTER TABLE "Deceased" ADD FOREIGN KEY ("FaithName") REFERENCES "Faith" ("FaithName");
-
-ALTER TABLE "Reservation" ADD FOREIGN KEY ("GraveId") REFERENCES "Grave" ("GraveId");
-
-ALTER TABLE "Reservation" ADD FOREIGN KEY ("UserId") REFERENCES "User" ("UserId");
-
-ALTER TABLE "Reservation" ADD FOREIGN KEY ("PaymentId") REFERENCES "Payment" ("PaymentId");
-
-ALTER TABLE "DeceasedHistory" ADD FOREIGN KEY ("DeceasedId") REFERENCES "Deceased" ("DeceasedId");
-
-ALTER TABLE "DeceasedHistory" ADD FOREIGN KEY ("UserId") REFERENCES "User" ("UserId");
-
-ALTER TABLE "Subscription" ADD FOREIGN KEY ("GraveId") REFERENCES "Grave" ("GraveId");
-
-ALTER TABLE "Subscription" ADD FOREIGN KEY ("ServiceId") REFERENCES "Service" ("ServiceId");
-
-ALTER TABLE "Subscription" ADD FOREIGN KEY ("PaymentId") REFERENCES "Payment" ("PaymentId");
-
-ALTER TABLE "Purchase" ADD FOREIGN KEY ("UserId") REFERENCES "User" ("UserId");
-
-ALTER TABLE "Purchase" ADD FOREIGN KEY ("ServiceId") REFERENCES "Service" ("ServiceId");
+ALTER TABLE "Sector" ADD FOREIGN KEY ("FK_FaithName") REFERENCES "Faith" ("FaithName");
+ALTER TABLE "Monument" ADD FOREIGN KEY ("FK_SectorId") REFERENCES "Sector" ("SectorId");
+ALTER TABLE "Gate" ADD FOREIGN KEY ("FK_SectorId") REFERENCES "Sector" ("SectorId");
+ALTER TABLE "Camera" ADD FOREIGN KEY ("FK_SectorId") REFERENCES "Sector" ("SectorId");
+ALTER TABLE "Grave" ADD FOREIGN KEY ("FK_SectorId") REFERENCES "Sector" ("SectorId");
+ALTER TABLE "Chapel" ADD FOREIGN KEY ("FK_SectorId") REFERENCES "Sector" ("SectorId");
+ALTER TABLE "Funeral" ADD FOREIGN KEY ("FK_ChapelId") REFERENCES "Chapel" ("ChapelId");
+ALTER TABLE "Funeral" ADD FOREIGN KEY ("FK_DeceasedId") REFERENCES "Deceased" ("DeceasedId");
+ALTER TABLE "Funeral" ADD FOREIGN KEY ("FK_PaymentId") REFERENCES "Payment" ("PaymentId");
+ALTER TABLE "Deceased" ADD FOREIGN KEY ("FK_GraveId") REFERENCES "Grave" ("GraveId");
+ALTER TABLE "Deceased" ADD FOREIGN KEY ("FK_FaithName") REFERENCES "Faith" ("FaithName");
+ALTER TABLE "Reservation" ADD FOREIGN KEY ("FK_GraveId") REFERENCES "Grave" ("GraveId");
+ALTER TABLE "Reservation" ADD FOREIGN KEY ("FK_UserId") REFERENCES "User" ("UserId");
+ALTER TABLE "Reservation" ADD FOREIGN KEY ("FK_PaymentId") REFERENCES "Payment" ("PaymentId");
+ALTER TABLE "DeceasedHistory" ADD FOREIGN KEY ("FK_DeceasedId") REFERENCES "Deceased" ("DeceasedId");
+ALTER TABLE "Subscription" ADD FOREIGN KEY ("FK_GraveId") REFERENCES "Grave" ("GraveId");
+ALTER TABLE "Subscription" ADD FOREIGN KEY ("FK_ServiceId") REFERENCES "Service" ("ServiceId");
+ALTER TABLE "Subscription" ADD FOREIGN KEY ("FK_PaymentId") REFERENCES "Payment" ("PaymentId");
+ALTER TABLE "Purchase" ADD FOREIGN KEY ("FK_UserId") REFERENCES "User" ("UserId");
+ALTER TABLE "Purchase" ADD FOREIGN KEY ("FK_ServiceId") REFERENCES "Service" ("ServiceId");
